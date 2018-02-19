b530e5cb8886f8b90f11f3ed695bcf17a6113d70
Josh Vogel <joshvogel6@comcast.net>
2018-02-19 14:19:11 -0800

    just put it back the way it was for now.

--- resources/views/emails/comment.blade.php
+++ resources/views/emails/comment.blade.php
@@ -18,7 +18,7 @@
 <p>Status: {{ $ticket_status }}</p>
 
 <p>
-    You can view the ticket at any time at {{ url(config('app.support_ticket_url')).'/home/tickets'. $ticket_id }}
+    You can view the ticket at any time at {{ url('home/tickets'. $ticket_id)}}
 </p>
 
 </body>
